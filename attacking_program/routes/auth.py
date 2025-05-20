6f197c7f7f775b290a83f85b5eff121c2432d2a4
Michel Francisco <michel.francisco@thalesgroup.com>
2025-05-20 15:55:26 +0200

    [FIX] Authentication variable never accessed

--- attacking_program/routes/auth.py
+++ attacking_program/routes/auth.py
@@ -1,20 +1,18 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
-from utils.state import PASSWORD, authenticated, app
+import utils.state as state
 
 auth_bp = Blueprint('auth', __name__)
 
 @auth_bp.route('/')
 def login():
-    from utils.state import authenticated
-    if authenticated:
+    if state.authenticated:
         return redirect(url_for('dashboard.dashboard'))
     return render_template("login.html")
 
 @auth_bp.route('/auth', methods=['POST'])
 def auth():
-    from utils.state import authenticated
-    if request.form.get('password') == PASSWORD:
-        authenticated = True
+    if request.form.get('password') == state.PASSWORD:
+        state.authenticated = True
         return redirect(url_for('dashboard.dashboard'))
     flash("❌ Mot de passe incorrect.")
     return redirect(url_for('auth.login'))
