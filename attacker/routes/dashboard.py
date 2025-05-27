11ba454ffbecba06a26df7ce32f5974ec0d93f0a
Thibault Colcomb <thibault.colcomb@epita.fr>
2025-05-27 15:15:43 +0200

    [FIX] Dashbord displaying "Not connected" for a moment when auth

--- attacker/routes/dashboard.py
+++ attacker/routes/dashboard.py
@@ -1,9 +1,12 @@
 from app import app
 from flask import render_template, redirect, url_for, session
 from datetime import datetime
+from routes.api import getClientInfos
+import config as cfg
 
 # -------------------------------- DASHBOARD -------------------------------- #
 
 @app.route('/dashboard')
 def dashboard():
-    return render_template("dashboard.html")
+    clientInfos = getClientInfos()
+    return render_template("dashboard.html", clientInfos=clientInfos)
